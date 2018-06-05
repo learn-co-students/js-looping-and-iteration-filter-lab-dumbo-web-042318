7806bc8eb216c9cf0fd363748f5bed5afe321b90
cjunks94 <cjunks94@gmail.com>
2018-06-05 08:09:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(arr, name){
+  return arr.filter(function (findName){ //filter by funtion
+    return findName.toLowerCase() === name.toLowerCase()
+  });
+}
+
+function fuzzyMatch(arr, name){
+  return arr.filter(function(findName){
+    return findName.slice(0,name.length).toLowerCase() === name.toLowerCase()
+  });
+}
+
+function matchName(arr, name){
+return arr.filter( function (driver){ //filter by funtion
+  return driver.name.toLowerCase() === name.toLowerCase();
+});
+}
