6d223b704887abbb2d06e7cf000b3ac20dfef8f2
SnowSurfer <drinkaziu@gmail.com>
2018-06-05 13:18:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+  return drivers.filter(function(name) {
+    return name.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(name) {
+    return name.slice(0, string.length).toLowerCase() === string.toLowerCase();
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(nameObj) {
+    return nameObj.name === string;
+  });
+}
